1e63d97e9125fb360c3525c26c3a972e87ac883e
James Budge <thebudgie@gmail.com>
2019-04-26 11:28:22 +0100

    adds index page

---
+++ controllers/tags_controller.rb
@@ -0,0 +1,10 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('../models/tag.rb')
+
+also_reload( '../models/*' )
+
+get '/tags' do
+  @tags = Tag.all
+  erb(:"tags/index")
+end
--- spending_tracker.rb
+++ spending_tracker.rb
@@ -4,6 +4,8 @@ require_relative('controllers/tag_controller.rb')
 require_relative('controllers/merchant_controller.rb')
 require_relative('controllers/transaction_controller.rb')
 
+also_reload('../models/*')
+
 get '/' do
   erb(:index)
 end
