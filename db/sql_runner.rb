a2f9067117d774122efb1ed19c4960b8b45c44bf
James Budge <thebudgie@gmail.com>
2019-06-11 12:25:51 +0100

    meh

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,7 +1,6 @@
 require('pg')
 
 class SqlRunner
-
   def self.run(sql, values = [])
     begin
       db = PG.connect({dbname: 'd67ktaeup407o', host: 'ec2-23-21-186-85.compute-1.amazonaws.com', user: 'wiygqqlozzgbku'}, password: '09a973d041643c3dec22de6cd9b09e876b885f38d5f44615223f06b2d9c21b06')
